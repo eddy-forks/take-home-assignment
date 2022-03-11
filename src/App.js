18b4f49ef4556e1518da599d9564f58fe767fd6c
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-03-11 14:25:33 -0500

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,68 +1,73 @@
-import './App.css';
-import React from 'react';
+import './App.css'
+import React from 'react'
 
 function App() {
-  const [textInput, setTextInput] = React.useState('Here is some example text.');
-  const [conversionMode, setConversionMode] = React.useState('lowercase');
-  const [textOutput, setTextOutput] = React.useState('');
+  const [textInput, setTextInput] = React.useState('Here is some example text.')
+  const [conversionMode, setConversionMode] = React.useState('lowercase')
+  const [textOutput, setTextOutput] = React.useState('')
 
   const handleRadioChange = event => {
-    setConversionMode(event.target.value);
+    setConversionMode(event.target.value)
   }
 
   const handleTextareaChange = event => {
-    setTextInput(event.target.value);
-  };
+    setTextInput(event.target.value)
+    setTextOutput('')
+  }
 
   const handleSubmit = event => {
-    event.preventDefault();
-    setTextOutput('Your formatted text will go here!')
-  };
+    event.preventDefault()
+    conversionMode === 'uppercase'
+      ? setTextOutput(textInput.toUpperCase())
+      : setTextOutput(textInput.toLowerCase())
+  }
 
   return (
-    <div className="App">
+    <div className='App'>
       <header>
         <h1>Career Lab text-case converter</h1>
       </header>
       <form onSubmit={handleSubmit}>
-        <div className="form-control form-control__text">
-          <label htmlFor="text">Text to be converted:</label>
+        <div className='form-control form-control__text'>
+          <label htmlFor='text'>Text to be converted:</label>
           <textarea
-            id="text"
+            id='text'
             onChange={handleTextareaChange}
             value={textInput}
           />
         </div>
-        <div className="form-control form-control__radio">
+        <div className='form-control form-control__radio'>
           <input
-            type="radio"
-            name="conversion"
-            id="conversion-0"
-            value="lowercase"
-            checked={conversionMode === "lowercase"}
+            type='radio'
+            name='conversion'
+            id='conversion-0'
+            value='lowercase'
+            checked={conversionMode === 'lowercase'}
             onChange={handleRadioChange}
           />
-          <label htmlFor="conversion-0">Convert text to lowercase</label>
+          <label htmlFor='conversion-0'>Convert text to lowercase</label>
         </div>
-        <div className="form-control form-control__radio">
+        <div className='form-control form-control__radio'>
           <input
-            type="radio"
-            name="conversion"
-            id="conversion-1"
-            value="uppercase"
-            checked={conversionMode === "uppercase"}
+            type='radio'
+            name='conversion'
+            id='conversion-1'
+            value='uppercase'
+            checked={conversionMode === 'uppercase'}
             onChange={handleRadioChange}
           />
-          <label htmlFor="conversion-1">Convert text to uppercase</label>
+          <label htmlFor='conversion-1'>Convert text to uppercase</label>
         </div>
-        <button type="button">Submit</button>
-        <div className="form-control form-control__text u-mt-3">
-          <label htmlFor="result">Converted text:</label>
-          <output id="result" class="result">{textOutput}</output>
+        <button type='submit'>Submit</button>
+        <div className='form-control form-control__text u-mt-3'>
+          <label htmlFor='result'>Converted text:</label>
+          <output id='result' className='result'>
+            {textOutput}
+          </output>
         </div>
       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
